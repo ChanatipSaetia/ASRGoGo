8fae6d101eae3f07690bc296f484ea461e1a7a3b
魂火SoulFire <chanatippung@gmail.com>
2017-05-09 17:42:08 +0700

    add model to repo

--- dev/test.py
+++ dev/test.py
@@ -9,25 +9,25 @@ countT = pickle.load(open("countT.sav","rb"))
 Y = [] #list of label
 df = [] #dataset of words each row
 #----------------------------------get data from dev set time.txt----------------------------------
-texts = open("dev_set.txt",'r', encoding='utf-8')
+texts = open("test_set.txt",'r', encoding='utf-8')
 for text in texts :
     df.append(text.replace('\n',''))
     Y.append(0)
 
 #----------------------------------get data from dev what time.txt----------------------------------
-texts = open("dev_time.txt",'r', encoding='utf-8')
+texts = open("test_time.txt",'r', encoding='utf-8')
 for text in texts :
     df.append(text.replace('\n',''))
     Y.append(1)
 
 #----------------------------------get data from dev postpone .txt----------------------------------
-texts = open("dev_postpone.txt",'r', encoding='utf-8')
+texts = open("test_postpone.txt",'r', encoding='utf-8')
 for text in texts :
     df.append(text.replace('\n',''))
     Y.append(2)
 
 #----------------------------------get data from dev postpone .txt----------------------------------
-texts = open("dev_task.txt",'r', encoding='utf-8')
+texts = open("test_task.txt",'r', encoding='utf-8')
 for text in texts :
     df.append(text.replace('\n',''))
     Y.append(3)
@@ -38,6 +38,8 @@ count = countT.transform(df)
 #-----------------------------------apply model------------------------------------------------
 y_pred = model.predict(count)
 f1 = f1_score(Y, y_pred, average='macro')
+print("label: " + str(Y))
+print("predicted label: " + str(y_pred))
 print("number of dev: " + str(len(y_pred)))
 print("f1 score: " + str(f1))
 score = accuracy_score(Y, y_pred)
